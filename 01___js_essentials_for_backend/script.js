7382a1d68f1dc6a24376417d58ff1de95d20ed55
Montajur Rahman <montajurrdev@gmail.com>
2026-04-26 14:08:05 +0600

    first commit

---
+++ 01___js_essentials_for_backend/script.js
@@ -0,0 +1,16 @@
+// Fundamentals of JavaScript
+// arrays and object
+// functions return
+// async js coding
+// foreach map filter find indexof
+
+
+var arr = [1,2,3,4,"hello",{},function(){},[]];
+
+arr.forEach((data, i)=>{
+    console.log(data, i);
+    
+    
+
+})
+
