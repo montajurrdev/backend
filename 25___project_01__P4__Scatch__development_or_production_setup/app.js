c4b2f81a02306bbb6f64afdf30297ff964545db3
Montajur Rahman <montajurrdev@gmail.com>
2026-05-25 17:21:10 +0600

    debugger, config setup for secure uri, create owner

---
+++ 25___project_01__P4__Scatch__development_or_production_setup/app.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const app = express()
+const cookieParser = require('cookie-parser');
+const path = require('path');
+
+// router
+const ownersRouter = require("./routes/ownersRouter")
+const usersRouter = require("./routes/usersRouter")
+const productsRouter = require("./routes/productsRouter")
+
+// db connection
+const db = require("./config/mongoose-connection")
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.set("view engine", "ejs")
+
+// routes: 
+app.use("/owners", ownersRouter)  // means: owners related sob req ownersRouter a patay dao
+app.use("/users", usersRouter)
+app.use("/products", productsRouter)
+
+app.listen(3000)
---
+++ 25___project_01__P4__Scatch__development_or_production_setup/routes/ownersRouter.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const router = express.Router(); // it handles routing
+
+// model
+const ownerModel = require("../models/owner-model");
+
+// console.log(process.env.NODE_ENV);
+
+// this routes will run till env variable is development
+if (process.env.NODE_ENV === "development") {
+  router.post("/create", async function (req, res) {
+    let owners = await ownerModel.find();
+    if (owners.length > 0){
+        return res
+        .status(500)
+        .send("You don't have permission to create a new owner.");
+    }
+    
+
+    
+    let {fullname, email, password} = req.body;
+
+     let createdOwner = await ownerModel.create({
+       fullname,
+       email,
+       password,
+
+     });
+    
+    res.status(201).send(createdOwner)
+
+  });
+}
+
+router.get("/", function (req, res) {
+  res.send("hey it's working");
+});
+
+module.exports = router;
